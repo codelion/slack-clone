f295e4cae6ec203344583970781fd4658598da35
Manish Jha <manishjhazyx@gmail.com>
2022-01-31 18:22:23 +0530

    initial commit

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD0eW_T5YaV1C0Pl_eVs5IfZS4PCFn6hcc",
+  authDomain: "manish-slack-clone.firebaseapp.com",
+  projectId: "manish-slack-clone",
+  storageBucket: "manish-slack-clone.appspot.com",
+  messagingSenderId: "738633377017",
+  appId: "1:738633377017:web:bac7205f6405def673ee28",
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
